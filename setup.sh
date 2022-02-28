5a37ae64741cd30dd402607bee1104295a562e29
Andry Luthfi <andryluthfi@Andrys-MacBook-Pro.local>
2022-02-28 19:24:35 +0700

    initialize

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+brew install spectacle --cask
+brew install stats --cask
+
+brew install git
+brew install brew-php-switcher
+
+brew tap shivammathur/php
+brew install shivammathur/php/php@7.1
+brew install shivammathur/php/php@7.4
+brew install shivammathur/php/php@8.0
+brew install shivammathur/php/php@8.1
+
+
+ln -s ~/.config/php/overide-php.ini /usr/local/etc/php/8.0/conf.d/overide-php.ini
+ln -s ~/.config/php/overide-php.ini /usr/local/etc/php/7.1/conf.d/overide-php.ini
