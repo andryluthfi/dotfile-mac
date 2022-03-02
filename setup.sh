fd2f75e9b1f7fc01cb2c702655d1da00040e96d6
Andry Luthfi <andryluthfi@Andrys-MacBook-Pro.local>
2022-03-03 02:30:45 +0700

    update config and add setup.sh

--- setup.sh
+++ setup.sh
@@ -12,6 +12,8 @@ brew install shivammathur/php/php@7.4
 brew install shivammathur/php/php@8.0
 brew install shivammathur/php/php@8.1
 
+brew install --cask openvpn-connect
+
 
 ln -s ~/.config/php/overide-php.ini /usr/local/etc/php/8.0/conf.d/overide-php.ini
 ln -s ~/.config/php/overide-php.ini /usr/local/etc/php/7.1/conf.d/overide-php.ini
