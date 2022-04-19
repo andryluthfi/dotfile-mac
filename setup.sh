96589174591c6e79bb8e97c2f0e173671ab3c365
Andry Luthfi <andrylutv@gmail.com>
2022-04-19 18:38:37 +0700

    add cocoapod

--- setup.sh
+++ setup.sh
@@ -18,6 +18,8 @@ brew install shivammathur/php/php@8.0
 brew install shivammathur/php/php@8.1
 brew install mongodb-community@5.0
 
+sudo gem install cocoapods
+
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 brew install watchman
 
