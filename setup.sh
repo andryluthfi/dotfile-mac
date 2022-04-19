d704d26957ceb585ac56dabe8c36a1601bbc7a97
Andry Luthfi <andrylutv@gmail.com>
2022-04-19 15:40:04 +0700

    add postmant and tidying some lines

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-brew install spectacle --cask
-brew install stats --cask
+brew install --cask spectacle
+brew install --cask stats
+brew install --cask postman
+brew install --cask openvpn-connect
 
 brew install git
 brew install bat
@@ -18,8 +20,6 @@ brew install mongodb-community@5.0
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 brew install watchman
-
-brew install --cask openvpn-connect
 
 ln -s ~/.config/php/overide-php.ini /usr/local/etc/php/8.0/conf.d/overide-php.ini
 ln -s ~/.config/php/overide-php.ini /usr/local/etc/php/7.1/conf.d/overide-php.ini
