5615ab34b134f9725746ab834d84072d99783640
Andry Luthfi <andrylutv@gmail.com>
2022-04-19 15:24:27 +0700

    add mongodb and nvm, watchman

--- setup.sh
+++ setup.sh
@@ -7,13 +7,20 @@ brew install git
 brew install brew-php-switcher
 
 brew tap shivammathur/php
+brew tap mongodb/brew
+
 brew install shivammathur/php/php@7.1
 brew install shivammathur/php/php@7.4
 brew install shivammathur/php/php@8.0
 brew install shivammathur/php/php@8.1
+brew install mongodb-community@5.0
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+brew install watchman
 
 brew install --cask openvpn-connect
 
-
 ln -s ~/.config/php/overide-php.ini /usr/local/etc/php/8.0/conf.d/overide-php.ini
 ln -s ~/.config/php/overide-php.ini /usr/local/etc/php/7.1/conf.d/overide-php.ini
+
+source ~/.zshrc
